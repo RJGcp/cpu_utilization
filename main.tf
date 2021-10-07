b78e7673069cd29e51e3c42189e03d7036e2b60e
RJ <ravichanderkt@gmail.com>
2021-10-07 18:04:40 +0000

    Jesus Worked

--- main.tf
+++ main.tf
@@ -18,13 +18,14 @@ data "google_monitoring_notification_channel" "Alerts" {
 #   display_name = "king-of-kings"
 # }
 
-resource "google_monitoring_group" "parent" {
-  display_name = "New Test SubGroup"
-  filter = "resource.metadata.region=\"us-east1\""
+resource "google_monitoring_group" "check" {
+  project = var.project
+  display_name = "rj-group"
+  filter       = "resource.metadata.region=\"europe-west2\""
 }
 
 resource "google_monitoring_alert_policy" "cpu_utilization" {
-  depends_on = [google_monitoring_notification_channel.basic,google_monitoring_group.parent]
+  depends_on = [google_monitoring_notification_channel.basic]
   project    = var.project
   enabled    = "true"
 
@@ -34,7 +35,7 @@ resource "google_monitoring_alert_policy" "cpu_utilization" {
   conditions {
     display_name = "Critical: Project[${var.project}] GCE instance CPU Utilization is 0%"
     condition_absent {
-      filter          = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\" AND group.id=\"${google_monitoring_group.parent.id}\""
+      filter          = "metric.type=\"compute.googleapis.com/instance/cpu/usage_time\" AND resource.type=\"gce_instance\""
       duration        = "3600s"
     # condition_threshold {
     #   filter          = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
@@ -42,10 +43,10 @@ resource "google_monitoring_alert_policy" "cpu_utilization" {
     #   comparison = ""
       aggregations {
         alignment_period   = "300s"
-        per_series_aligner = "ALIGN_NONE"
-        group_by_fields = [
-          "resource.project_id", "resource.label.instance_name"
-        ]
+        per_series_aligner = "ALIGN_DELTA"
+        # group_by_fields = [
+        #   "resource.project_id", "resource.label.instance_name"
+        # ]
       }
     }  
   }
